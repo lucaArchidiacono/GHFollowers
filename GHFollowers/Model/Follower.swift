30d5dcd3e4d7f1d1f59d23f20e6e4574f80f1658
lucaArchidiacono <lucaarchidiacono@gmail.com>
2020-10-26 19:02:26 +0100

    Prepared models for the network call to the Github REST API

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Luca Archidiacono on 26.10.20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
